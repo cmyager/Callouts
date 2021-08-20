08dc11f377e0873cc253b8812f1b68daf7e41f32
Lance Dice <lancedice@gmail.com>
2021-08-19 22:52:32 -0700

    New Event page start

---
+++ Pages/NewEvent.razor.cs
@@ -0,0 +1,23 @@
+﻿using BungieSharper.Entities;
+using BungieSharper.Entities.Destiny;
+using BungieSharper.Entities.Destiny.Responses;
+using BungieSharper.Entities.User;
+using Callouts.Data;
+using Callouts.DataContext;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blazorise;
+
+namespace Callouts.Pages
+{
+    public partial class NewEvent : ComponentBase
+    {
+        protected string eventName;
+        protected string eventDiscription;
+        
+    }
+}
